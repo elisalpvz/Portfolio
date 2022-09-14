796816b7e20a0616bbad8de372d50ff10fb47085
Elisa <104301924+elisalpvz@users.noreply.github.com>
2022-09-14 16:51:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+alert("Hola 😊");
+
+if (confirm("Estoy trabajando en cosas nuevas 💻 " +  "Nos vemos pronto!")) {
+    txt = "You pressed OK!";
+  } 
+  else {
+    txt = "You pressed Cancel!";
+  }
+
